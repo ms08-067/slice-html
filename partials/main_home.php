524e04cc12436dd87773a40742d2200a7936174a
ms08-067 <hunguit@yahoo.com>
2020-03-20 09:12:17 +0700

    update

--- partials/main_home.php
+++ partials/main_home.php
@@ -27,31 +27,97 @@
 			<div class="row">
     			<div class="col col-lg-12">
     				<div class="section-title">
-    					<h1 class="text-center">NEWS & EVENTS</h1>
+    					<h1 class="text-center"><span>NEWS & EVENTS</span></h1>
     				</div>
 
     				<div class="list-card col-lg-12">
     					<div class="row">
     						<div class="col col-lg-4">
-	    						<img class="img-fluid" src="./assets/img/card/card-1.jpg">
+	    						<div class="card">
+								  <img class="card-img-top" src="./assets/img/card/news-1.jpg">
+								  <div class="card-body">
+								    <h5 class="card-title">Tour Confidential: Are Tiger Woods’ Masters</h5>
+								    <span class="txt-date">13/3/2019</span>
+								    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								    <hr/>
+									<a href="https://www.facebook.com/sharer/sharer.php?u=http://sgolf.vn" class="share float-left"><img class="icon-share" src="./assets/img/icon/icon-share.jpg"> Share</a>
+								    <a href="" class="read-more float-right">More >> </a>
+								  </div>
+								</div>
+
 	    					</div>
 	    					<div class="col col-lg-4">
-	    						<img class="img-fluid" src="./assets/img/card/card-1.jpg">
+	    						<div class="card">
+								  <img class="card-img-top" src="./assets/img/card/news-1.jpg">
+								  <div class="card-body">
+								    <h5 class="card-title">Tour Confidential: Are Tiger Woods’ Masters</h5>
+								    <span class="txt-date">13/3/2019</span>
+								    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								    <hr/>
+									<a href="https://www.facebook.com/sharer/sharer.php?u=http://sgolf.vn" class="share float-left"><img class="icon-share" src="./assets/img/icon/icon-share.jpg"> Share</a>
+								    <a href="" class="read-more float-right">More >> </a>
+								  </div>
+								</div>
+
 	    					</div>
 	    					<div class="col col-lg-4">
-	    						<img class="img-fluid" src="./assets/img/card/card-1.jpg">
+	    						<div class="card">
+								  <img class="card-img-top" src="./assets/img/card/news-1.jpg">
+								  <div class="card-body">
+								    <h5 class="card-title">Tour Confidential: Are Tiger Woods’ Masters</h5>
+								    <span class="txt-date">13/3/2019</span>
+								    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								    <hr/>
+									<a href="https://www.facebook.com/sharer/sharer.php?u=http://sgolf.vn" class="share float-left"><img class="icon-share" src="./assets/img/icon/icon-share.jpg"> Share</a>
+								    <a href="" class="read-more float-right">More >> </a>
+								  </div>
+								</div>
+
 	    					</div>
     					</div>
 
     					<div class="row">
     						<div class="col col-lg-4">
-	    						<img class="img-fluid" src="./assets/img/card/card-1.jpg">
+	    						<div class="card">
+								  <img class="card-img-top" src="./assets/img/card/news-1.jpg">
+								  <div class="card-body">
+								    <h5 class="card-title">Tour Confidential: Are Tiger Woods’ Masters</h5>
+								    <span class="txt-date">13/3/2019</span>
+								    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								    <hr/>
+									<a href="https://www.facebook.com/sharer/sharer.php?u=http://sgolf.vn" class="share float-left"><img class="icon-share" src="./assets/img/icon/icon-share.jpg"> Share</a>
+								    <a href="" class="read-more float-right">More >> </a>
+								  </div>
+								</div>
+
 	    					</div>
 	    					<div class="col col-lg-4">
-	    						<img class="img-fluid" src="./assets/img/card/card-1.jpg">
+	    						<div class="card">
+								  <img class="card-img-top" src="./assets/img/card/news-1.jpg">
+								  <div class="card-body">
+								    <h5 class="card-title">Tour Confidential: Are Tiger Woods’ Masters</h5>
+								    <span class="txt-date">13/3/2019</span>
+								    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								    <hr/>
+									<a href="https://www.facebook.com/sharer/sharer.php?u=http://sgolf.vn" class="share float-left"><img class="icon-share" src="./assets/img/icon/icon-share.jpg"> Share</a>
+								    <a href="" class="read-more float-right">More >> </a>
+								  </div>
+								</div>
+
 	    					</div>
 	    					<div class="col col-lg-4">
-	    						<img class="img-fluid" src="./assets/img/card/card-1.jpg">
+	    						<div class="card">
+								  <img class="card-img-top" src="./assets/img/card/news-1.jpg">
+								  <div class="card-body">
+								    <h5 class="card-title">Tour Confidential: Are Tiger Woods’ Masters</h5>
+								    <span class="txt-date">13/3/2019</span>
+								    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								    <hr/>
+									<a href="https://www.facebook.com/sharer/sharer.php?u=http://sgolf.vn" class="share float-left"><img class="icon-share" src="./assets/img/icon/icon-share.jpg"> Share</a>
+								    <a href="" class="read-more float-right">More >> </a>
+								  </div>
+								</div>
+
 	    					</div>
     					</div>
 
@@ -63,11 +129,101 @@
 	</div>
 
 	<div class="partner-home">
-		<img style="height: 490px;width: 100%;" src="./assets/img/partner-home.jpg" />
+		<!-- <img style="height: 490px;width: 100%;" src="./assets/img/partner-home.jpg" /> -->
+		<div class="container">
+			<div class="row">
+		    	<div class="col col-lg-2 center">
+		    		<a href="javascript:void()"><img class="img-fluid img-partner" src="./assets/img/partner/partner-1.jpg"></a>
+		    	</div>
+		    	<div class="col col-lg-2 center">
+		    		<a href="javascript:void()"><img class="img-fluid img-partner" src="./assets/img/partner/partner-2.jpg"></a>
+		    	</div>
+		    	<div class="col col-lg-2 center">
+		    		<a href="javascript:void()"><img class="img-fluid img-partner" src="./assets/img/partner/partner-3.jpg"></a>
+		    	</div>
+		    	<div class="col col-lg-2 center">
+		    		<a href="javascript:void()"><img class="img-fluid img-partner" src="./assets/img/partner/partner-4.jpg"></a>
+		    	</div>
+		    	<div class="col col-lg-2 center">
+		    		<a href="javascript:void()"><img class="img-fluid img-partner" src="./assets/img/partner/partner-5.jpg"></a>
+		    	</div>
+		    	<div class="col col-lg-2 center">
+		    		<a href="javascript:void()"><img class="img-fluid img-partner" src="./assets/img/partner/partner-6.jpg"></a>
+		    	</div>
+			</div>
+
+			<div class="row">
+		    	<div class="col col-lg-2 center">
+		    		<a href="javascript:void()"><img class="img-fluid img-partner" src="./assets/img/partner/partner-7.jpg"></a>
+		    	</div>
+		    	<div class="col col-lg-2 center">
+		    		<a href="javascript:void()"><img class="img-fluid img-partner" src="./assets/img/partner/partner-8.jpg"></a>
+		    	</div>
+		    	<div class="col col-lg-2 center">
+		    		<a href="javascript:void()"><img class="img-fluid img-partner" src="./assets/img/partner/partner-9.jpg"></a>
+		    	</div>
+		    	<div class="col col-lg-2 center">
+		    		<a href="javascript:void()"><img class="img-fluid img-partner" src="./assets/img/partner/partner-10.jpg"></a>
+		    	</div>
+		    	<div class="col col-lg-2 center">
+		    		<a href="javascript:void()"><img class="img-fluid img-partner" src="./assets/img/partner/partner-11.jpg"></a>
+		    	</div>
+		    	<div class="col col-lg-2 center">
+		    		<a href="javascript:void()"><img class="img-fluid img-partner" src="./assets/img/partner/partner-12.jpg"></a>
+		    	</div>
+			</div>
+
+
+			<div class="row">
+		    	<div class="col col-lg-2 center">
+		    		<a href="javascript:void()"><img class="img-fluid img-partner" src="./assets/img/partner/partner-13.jpg"></a>
+		    	</div>
+		    	<div class="col col-lg-2 center">
+		    		<a href="javascript:void()"><img class="img-fluid img-partner" src="./assets/img/partner/partner-14.jpg"></a>
+		    	</div>
+		    	<div class="col col-lg-2 center">
+		    		<a href="javascript:void()"><img class="img-fluid img-partner" src="./assets/img/partner/partner-15.jpg"></a>
+		    	</div>
+		    	<div class="col col-lg-2 center">
+		    		<a href="javascript:void()"><img class="img-fluid img-partner" src="./assets/img/partner/partner-16.jpg"></a>
+		    	</div>
+		    	<div class="col col-lg-2 center">
+		    		<a href="javascript:void()"><img class="img-fluid img-partner" src="./assets/img/partner/partner-17.jpg"></a>
+		    	</div>
+		    	<div class="col col-lg-2 center">
+		    		<a href="javascript:void()"><img class="img-fluid img-partner" src="./assets/img/partner/partner-18.jpg"></a>
+		    	</div>
+			</div>
+		</div>	    
 	</div>
 
 	<div class="about-us">
-		<img style="height: 408px;width: 100%;" src="./assets/img/aboutus.jpg" />
+		<div class="container">
+			<div class="row">
+    			<div class="col col-lg-12">
+    				<div class="section-title">
+    					<h1 class="text-center"><span>ABOUT US</span></h1>
+    				</div>
+    				<div class="row">
+    					<div class="col col-lg-1"> </div>
+    					<div class="col col-lg-10 content-about-us">
+    					<p>Unlike most ball games, golf does not require a standardized playing area. The game takes place on a course arranged according to a predefined cycle of 09 holes or 18 holes (or holes). Each hole on the course comes with a tee, and a putting green includes a hole (10.79 cm wide). In the middle of the two areas, there are other standard terrain types such as fairway (the area between tee box and putting green), rough (long grass), sand pits, and obstacles (water, rocks, bushes). However, each hole area on the course has a different design and layout.</p>
+    					<p>There are two main types of golf competition. In stroke play, the golfer tries to get the lowest score; in individual match play or the team tries to win more holes than the opponent. Stroke play is the most popular mode.</p>
+    					<p>Unlike most ball games, golf does not require a standardized playing area. The game takes place on a course arranged according to a predefined cycle of 09 holes or 18 holes (or holes). Each hole on the course comes with a tee, and a putting green includes a hole (10.79 cm wide). In the middle of the two areas, there are other standard terrain types such as fairway (the area between tee box and putting green), rough (long grass), sand pits, and obstacles (water, rocks, bushes). However, each hole area on the course has a different design and layout.</p>
+    					</div>
+    					<div class="col col-lg-1"></div>
+    				</div>
+    				<div class="row">
+    					<div class="col col-lg-1"> </div>
+    					<div class="col col-lg-10 center">
+    						<h1 class="center title-statistic">5000<br/><span class="txt-statistic">membership</span></h1>
+    						<h1 class="center title-statistic">200<br/><span class="txt-statistic">club</span></h1>
+    						<h1 class="center title-statistic">60<br/><span class="txt-statistic">golf course</span></h1>
+    					</div>
+    				</div>		
+    			</div>
+    		</div>
+    	</div>			
 	</div>
 
 </div>
