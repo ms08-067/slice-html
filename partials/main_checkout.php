3ff33707a6a993fa64604ad4cda6f160e4872835
lich <thanhlichit1999@gmail.com>
2020-03-20 16:37:21 +0700

    web checkout

--- partials/main_checkout.php
+++ partials/main_checkout.php
@@ -1,41 +1,78 @@
 <div class="main-checkout">
-	<div class="container" style="background: white;">
+	<div class="container in-main-checkout" >
 		<div class="online-payment">
-			<h4 style="text-align: center;"><img  src="./assets/img/icontructuyen.jpg"  class="img-online-payment" />Thanh toán trực tuyến</h4>
+			<h4 class="h-img-checkout"><img  src="./assets/img/icontructuyen.jpg"  class="img-online-payment" />Thanh toán trực tuyến</h4>
 			<p class="text-p">6:30</p>
 		</div>
 		<div class="bill">
 			<table class="table">
 			  <thead>
 			    <tr class="tr-bill">
-			      <th scope="col" style="width: 18;"><button type="button" class="btn btn-danger btn-bill">Tee time</button></th>
-			      <th scope="col" style="width: 24%;"><button type="button" class="btn btn-danger btn-bill">Giá</button></th>
-			      <th scope="col" style="width: 24%;"><button type="button" class="btn btn-danger btn-bill">người chơi</button></th>
-			      <th scope="col" style="width: 34%;"><button type="button" class="btn btn-danger btn-bill">Thành tiền</button></th>
+			      <th scope="col" class="col-tr-bill-first"><button type="button" class="btn btn-danger btn-bill">Tee time</button></th>
+			      <th scope="col" class="col-tr-bill-second"><button type="button" class="btn btn-danger btn-bill">Giá</button></th>
+			      <th scope="col" class="col-tr-bill-third" ><button type="button" class="btn btn-danger btn-bill">người chơi</button></th>
+			      <th scope="col" class="col-tr-bill-fourth" ><button type="button" class="btn btn-danger btn-bill-fourth">Thành tiền</button></th>
 			    </tr>
 			  </thead>
 			  
 			  <tbody>
-			    <tr>
+			    <tr class="tr-bill">
 			      <th>07:00</th>
-			      <td>Mar</td>
-			      <td>Otto</td>
-			      <td>@mdo</td>
+			      <th>2,000,000 VND</th>
+			      <th>4</th>
+			      <th>8,000,000 VND</th>
 			    </tr>
-			    <tr>
-			      <th scope="row">2</th>
-			      <td>Jacob</td>
-			      <td>Thornton</td>
-			      <td>@fat</td>
+			    <tr class="tr-bill">
+			      <th>Tổng</th>
+			      <th> </th>
+			      <th>4</th>
+			      <th>8,000,000 VND</th>
 			    </tr>
-			    <tr>
-			      <th scope="row">3</th>
-			      <td>Larry</td>
-			      <td>the Bird</td>
-			      <td>@twitter</td>
+			    <tr class="tr-bill">
+			      <th class="th-bill">Tổng</th>
+			      <th></th>
+			      <th></th>
+			      <th>8,000,000 VND</th>
 			    </tr>
 			  </tbody>
 			</table>
 		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4">
+				<form>
+				  <div class="form-group">
+				    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Họ tên(*)">
+				  </div>
+				</form>
+			</div>
+			<div class="col-sm-4">
+				<form >
+					<div class="form-group">
+					    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Điện thoại(*)">
+					</div>
+				</form>
+			</div>
+			<div class="col-sm-4">
+				<form >
+					<div class="form-group">
+					    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Email(*)">
+					</div>
+				</form>
+			</div>
+		</div>
+		<div class="row come-back-pay">
+			<div class="col-sm-4"></div>
+			<div class="col-sm-4">
+				<button type="button" class="btn btn-danger btn-bill">Quay lại</button>
+			</div>
+			<div class="col-sm-4">
+				<button type="button" class="btn btn-danger btn-bill-fourth">Thanh toán</button>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		
 	</div>
 </div>
