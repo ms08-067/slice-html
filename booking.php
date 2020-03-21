8edc76ff8894d20768ecdb4f7b3ad465a35e7e31
ms08-067 <hunguit@yahoo.com>
2020-03-21 11:52:16 +0700

    update

--- booking.php
+++ booking.php
@@ -22,6 +22,7 @@
     	<?php include("partials/footer.php"); ?>
     </div>
 </div><!-- end div.wrapper -->
+
 <script src="./assets/packages/jquery/jquery-3.4.1.min.js"></script>
 <script src="./assets/packages/bootstrap-4.0/js/bootstrap.js"></script>
 
