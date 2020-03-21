38dab534e29c138963d867175c5b1321171dba9e
ms08-067 <hunguit@yahoo.com>
2020-03-21 09:45:12 +0700

    update

--- checkout.php
+++ checkout.php
@@ -5,7 +5,7 @@
 <title>Checkout</title>
 <link href="./assets/packages/bootstrap-4.0/css/bootstrap.css" rel="stylesheet"/>
 
-<link href="./assets/css/style1.css" rel="stylesheet"/>
+<link href="./assets/css/style.css" rel="stylesheet"/>
 </head>
 
 <body>
