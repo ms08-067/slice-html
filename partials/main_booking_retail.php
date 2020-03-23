b70e9bebe15a52fb8ea838773d9879c67bc468c6
lich <thanhlichit1999@gmail.com>
2020-03-23 09:46:54 +0700

    update

--- partials/main_booking_retail.php
+++ partials/main_booking_retail.php
@@ -4,10 +4,10 @@
 			<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
 			  <div class="carousel-inner">
 			    <div class="carousel-item active">
-			      <img class="d-block w-100" src="./assets/img/booking_retail.jpg" alt="First slide">
+			      <img class="d-block w-100" src="./assets/img/slider/booking_retail.jpg" alt="First slide">
 			    </div>
 			    <div class="carousel-item">
-			      <img class="d-block w-100" src="./assets/img/booking_retail.jpg" alt="Second slide">
+			      <img class="d-block w-100" src="./assets/img/slider/booking_retail.jpg" alt="Second slide">
 			    </div>
 			  </div>
 			  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
