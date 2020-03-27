f98d36ad0a47d031c95f1410f43be77502afe0c8
lich <thanhlichit1999@gmail.com>
2020-03-27 08:17:44 +0700

    commit

--- partials/partner-booking.php
+++ partials/partner-booking.php
@@ -6,27 +6,25 @@
 					</div>
 				</div>
 			</div>
-
-
 			<div class="row">
-			    	<div class="col col-lg-2 center">
+			    	<div class="col col-lg-2 center img-logo-golf">
 			    		<a href="javascript:void()"><img class="img-fluid img-partner" src="./assets/img/partner/partner-1.jpg"></a>
 			    	</div>
-			    	<div class="col col-lg-2 center">
+			    	<div class="col col-lg-2 center img-logo-golf">
 			    		<a href="javascript:void()"><img class="img-fluid img-partner" src="./assets/img/partner/partner-2.jpg"></a>
 			    	</div>
-			    	<div class="col col-lg-2 center">
+			    	<div class="col col-lg-2 center img-logo-golf">
 			    		<a href="javascript:void()"><img class="img-fluid img-partner" src="./assets/img/partner/partner-3.jpg"></a>
 			    	</div>
-			    	<div class="col col-lg-2 center">
+			    	<div class="col col-lg-2 center img-logo-golf">
 			    		<a href="javascript:void()"><img class="img-fluid img-partner" src="./assets/img/partner/partner-4.jpg"></a>
 			    	</div>
-			    	<div class="col col-lg-2 center">
+			    	<div class="col col-lg-2 center img-logo-golf">
 			    		<a href="javascript:void()"><img class="img-fluid img-partner" src="./assets/img/partner/partner-5.jpg"></a>
 			    	</div>
-			    	<div class="col col-lg-2 center">
+			    	<div class="col col-lg-2 center img-logo-golf">
 			    		<a href="javascript:void()"><img class="img-fluid img-partner" src="./assets/img/partner/partner-6.jpg"></a>
 			    	</div>
 				</div>
-
+			</div>
 	    </div>
